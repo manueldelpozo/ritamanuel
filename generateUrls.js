d233ed4f522e9c3ac9f03e64bd01a04e4135bce0
Manuel del Pozo <mandpo@boozt.com>
2022-02-06 18:49:56 +0100

    fix invitations

--- generateUrls.js
+++ generateUrls.js
@@ -7,13 +7,13 @@ const PORT = 3000;
 const urlBase = 'https://manueldelpozo.github.io/ritamanuel';
 const urlLocal = `http://localhost:${PORT}`
 
-const getInvitationMessage = ({ guest, lang, url, isfromRita }) => {
-    const other = isfromRita ? 'Manu' : 'Rita';
+const getInvitationMessage = ({ guest, lang, url, isFromRita }) => {
+    const other = isFromRita ? 'Manu' : 'Rita';
     const messages = {
         es: `Hola ${guest} :) ${other} y yo tenemos el gusto de invitarte a nuestra boda 👰🤵. Haz click en el siguente enlace: ${url}`,
         es_pl: `Hola ${guest} :) ${other} y yo tenemos el gusto de invitaros a nuestra boda 👰🤵. Haz click en el siguente enlace: ${url}`,
-        pl: `Cześć ${guest} :) ${other} i ja mamy przyjemność zaprosić Państwa na nasz ślub 👰🤵. Kliknij poniższy link: ${url}`,
-        pl_pl: `Cześć ${guest} :) ${other} i ja mamy przyjemność zaprosić Państwa na nasz ślub 👰🤵. Kliknij poniższy link: ${url}`,
+        pl: `Cześć ${guest} :) ${other} i ja mamy przyjemność zaprosić ciebe na nasz ślub 👰🤵. Kliknij poniższy link: ${url}`,
+        pl_pl: `Cześć ${guest} :) ${other} i ja mamy przyjemność zaprosić was na nasz ślub 👰🤵. Kliknij poniższy link: ${url}`,
         en: `Hi ${guest} :) ${other} and I have the pleasure of inviting you to our wedding 👰🤵. Click on the following link: ${url}`,
         fr: `Coucou ${guest} :) ${other} et j'ai le plaisir de vous inviter à notre mariage 👰🤵. Cliquez sur le lien suivant: ${url}`,
     };
@@ -32,16 +32,16 @@ const getUrlListByLang = (lang, list) => list.map(guest => {
             lang,
             guest,
             url,
-            isfromRita: (['pl', 'pl_pl'].includes(lang) && guest !== 'Marcin') || (lang === 'en' && guest !== 'Filipa and Bruno'),
+            isFromRita: (['pl', 'pl_pl'].includes(lang) && guest !== 'Marcin') || (lang === 'en' && guest !== 'Filipa and Bruno'),
         }),
     };
 });
 
 const urlLists = Object.entries(guestsJson).map(([lang, list]) => getUrlListByLang(lang, list));
 
-fs.writeFile('public/guestUrls.json', JSON.stringify(urlLists, null, 2), (err) => {
+fs.writeFile('public/invitations.json', JSON.stringify(urlLists, null, 2), (err) => {
     if (err) throw err;
-    console.log('Data written to file');
+    console.log('Invitations written to file');
 });
 
 console.log('This is after the write call');
