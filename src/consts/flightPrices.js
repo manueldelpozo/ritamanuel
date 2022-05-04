cc93d95a5b6a2cc8b0c96d057a19bb5ce1b339ba
Manuel del Pozo <mandpo@boozt.com>
2022-05-04 20:54:25 +0200

    add more guests and update prices

--- src/consts/flightPrices.js
+++ src/consts/flightPrices.js
@@ -1,28 +1,28 @@
 const prices = {
     ryanair_poznan_alicante: {
-        aug25: 424,
-        aug28: 422,
-        sep1: 614,
+        aug25: 247,
+        aug28: 368,
+        sep1: 567,
     },
     ryanair_wroclaw_alicante: {
-        aug28: 279,
-        aug30: 317,
-        sep1: 427,
+        aug28: 312,
+        aug30: 251,
+        sep1: 423,
     },
     ryanair_alicante_poznan: {
-        sep4: 545,
-        sep8: 659,
-        sep11: 289,
+        sep4: 477,
+        sep8: 537,
+        sep11: 360,
     },
     ryanair_alicante_wroclaw: {
-        sep4: 420,
-        sep6: 424,
-        sep8: 289,
-        sep11: 289,
+        sep4: 500,
+        sep6: 420,
+        sep8: 329,
+        sep11: 364,
     },
     alsa_airport_murcia_eur: 9.65,
     hotel_eur: 60,
-    date: '29.03.2022',
+    date: '04.05.2022',
 }
 
 export default prices;
