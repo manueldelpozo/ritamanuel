1d78df96ef7cfc17c1d48561af435e50185bae3b
Manuel del Pozo <mandpo@boozt.com>
2022-07-17 13:02:20 +0200

    add more guests to list 2

--- generateUrls.js
+++ generateUrls.js
@@ -11,21 +11,31 @@ const PORT = 3000;
 const urlBase = 'https://manueldelpozo.github.io/ritamanuel';
 const urlLocal = `http://localhost:${PORT}`
 
-const getInvitationMessage = ({ guest, lang, url, isFromRita }) => {
+const getInvitationMessage = ({ guest, lang, url, isFromRita, messageType = 'invitations' }) => {
     const other = isFromRita ? 'Manu' : 'Rita';
     const messages = {
-        es: `Hola ${guest}! 🙂 ${other} y yo tenemos el gusto de invitarte a nuestra boda 👰🤵 Haz click en el siguente enlace: ${url} Avísame si tienes dificultad para ver la invitación. Saludos 👋`,
-        es_pl: `Hola ${guest}! 🙂 ${other} y yo tenemos el gusto de invitaros a nuestra boda 👰🤵 Haz click en el siguente enlace: ${url} Avisadme si tenéis dificultad para ver la invitación. Saludos 👋`,
-        pl: `Cześć ${guest}! 🙂 ${other} i ja mamy przyjemność zaprosić Ciebe na Nasz ślub 👰🤵 Kliknij poniższy link: ${url}`,
-        pl_pl: `Cześć ${guest}! 🙂 ${other} i ja mamy przyjemność zaprosić Was na Nasz ślub 👰🤵 Kliknij poniższy link: ${url}`,
-        en: `Hi ${guest}! 🙂 ${other} and I have the pleasure of inviting you to our wedding 👰🤵 Click on the following link: ${url}`,
-        fr: `Coucou ${guest}! 🙂 ${other} et moi avons le plaisir de vous inviter à notre mariage 👰🤵 Cliquez sur le lien suivant: ${url}`,
+        invitation: {
+            es: `Hola ${guest}! 🙂 ${other} y yo tenemos el gusto de invitarte a nuestra boda 👰🤵 Haz click en el siguente enlace: ${url} Avísame si tienes dificultad para ver la invitación. Saludos 👋`,
+            es_pl: `Hola ${guest}! 🙂 ${other} y yo tenemos el gusto de invitaros a nuestra boda 👰🤵 Haz click en el siguente enlace: ${url} Avisadme si tenéis dificultad para ver la invitación. Saludos 👋`,
+            pl: `Cześć ${guest}! 🙂 ${other} i ja mamy przyjemność zaprosić Ciebe na Nasz ślub 👰🤵 Kliknij poniższy link: ${url}`,
+            pl_pl: `Cześć ${guest}! 🙂 ${other} i ja mamy przyjemność zaprosić Was na Nasz ślub 👰🤵 Kliknij poniższy link: ${url}`,
+            en: `Hi ${guest}! 🙂 ${other} and I have the pleasure of inviting you to our wedding 👰🤵 Click on the following link: ${url}`,
+            fr: `Coucou ${guest}! 🙂 ${other} et moi avons le plaisir de vous inviter à notre mariage 👰🤵 Cliquez sur le lien suivant: ${url}`,
+        },
+        reminders: {
+            es: `Hola ${guest}! 🙂 Solo recordarte que ${other} y yo te esperamos el 3 de septiembre en nuestra boda 👰🤵 Si no encuentras la invitación con todos los detalles, aquí la tienes de nuevo: ${url}. Nos vemos 👋`,
+            es_pl: `Hola ${guest}! 🙂 Solo recordaros que ${other} y yo os esperamos el 3 de septiembre en nuestra boda 👰🤵 Si no encontráis la invitación con todos los detalles, aquí la tenéis de nuevo: ${url}. Nos vemos 👋`,
+            pl: `Cześć ${guest}! 🙂 Przypomnij tylko, że ${other} i ja czekamy na Ciebie 3 września na nasz ślub 👰🤵 Jeśli nie możesz znaleźć zaproszenia ze wszystkimi szczegółami, oto jest ponownie: ${url}. Do zobaczenia 👋`,
+            pl_pl: `Cześć ${guest}! 🙂 Przypomnij tylko, że ${other} i ja czekamy na Was 3 września na nasz ślub 👰🤵 Jeśli nie możecie znaleźć zaproszenia ze wszystkimi szczegółami, oto jest ponownie: ${url}. Do zobaczenia 👋`,
+            en: `Hi ${guest}! 🙂 Just remind you that ${other} and I are waiting for you on September 3 for our wedding 👰🤵 If you can't find the invitation with all the details, here it is again: ${url}. See you soon 👋`,
+            fr: `Coucou ${guest}! 🙂 Rappelez-vous simplement que ${other} vous attendons le 3 septembre pour notre mariage 👰🤵 Si vous ne trouvez pas l'invitation avec tous les détails, la revoilà: ${url}. À bientôt 👋`,
+        },
     };
 
-    return messages[lang];
+    return messages[messageType][lang];
 };
 
-const getUrlListByLang = (lang, list) => list.map(guest => {
+const getUrlListByLang = (lang, list, messageType) => list.map(guest => {
     const url = `${urlBase}/#/${lang}?guest=${encodeURIComponent(guest)}`;
 
     return {
@@ -38,11 +48,13 @@ const getUrlListByLang = (lang, list) => list.map(guest => {
             url,
             isFromRita: (['pl', 'pl_pl'].includes(lang) && guest !== 'Marcin')
                 || (lang === 'en' && !['Filipa and Bruno', 'Gabriela and Slavko'].includes(guest)),
+            messageType,
         }),
     };
 });
 
-const urlLists = Object.entries(guestsJson).map(([lang, list]) => getUrlListByLang(lang, list));
+const urlLists = Object.entries(guestsJson).map(([lang, list]) => getUrlListByLang(lang, list, 'invitations'));
+const urlListsReminders = Object.entries(guestsJson).map(([lang, list]) => getUrlListByLang(lang, list, 'reminders'));
 const hotelNightsList = Object.entries(hotelNightsJson).map(([guest, nights]) => ({ guest, nights }));
 const nightPrice = {
     1: 57,
@@ -50,14 +62,14 @@ const nightPrice = {
 };
 const breakfastPrice = 10;
 
-const generateBodyContent = (invitations, hotelNights) => {
+const generateBodyContent = (list, hotelNights) => {
     const html = `<!DOCTYPE html>`;
     const { document } = new JSDOM(html).window;
     const tmp = document.createElement('div');
-    const list = document.createElement('ul');
-    const listNights = document.createElement('ul');
+    const domList = document.createElement('ul');
+    const domListNights = document.createElement('ul');
 
-    invitations.flat().forEach(({ guest, lang, url, message }) => {
+    list.flat().forEach(({ guest, lang, url, message }) => {
         const listItem = document.createElement('li');
         const link = document.createElement('a');
         const copyBtn = document.createElement('button');
@@ -73,7 +85,7 @@ const generateBodyContent = (invitations, hotelNights) => {
         listItem.appendChild(guestText);
         listItem.appendChild(link);
         listItem.appendChild(copyBtn);
-        list.appendChild(listItem);
+        domList.appendChild(listItem);
     });
 
     hotelNights.flat().forEach(({ guest, nights }) => {
@@ -85,21 +97,20 @@ const generateBodyContent = (invitations, hotelNights) => {
         listItem.appendChild(guestText);
         total.appendChild(totalText);
         listItem.appendChild(total);
-        listNights.appendChild(listItem);
+        domListNights.appendChild(listItem);
     });
 
-    tmp.appendChild(list);
-    tmp.appendChild(listNights);
+    tmp.appendChild(domList);
+    tmp.appendChild(domListNights);
 
     return tmp.innerHTML;
 }
 
-const html = createHTML({
+const htmlInvitations = createHTML({
     title: 'Invitations',
     head: `<meta name="description" content="Invitations">
            <script type="text/javascript">
                function copy(message) {
-                    console.log(navigator)
                     if ('clipboard' in navigator) {
                         navigator.clipboard.writeText(message);
                     } else {
@@ -111,7 +122,23 @@ const html = createHTML({
     body: generateBodyContent(urlLists, hotelNightsList),
 });
 
-fs.writeFile('public/invitations.html', html, function (err) {
+const htmlReminders = createHTML({
+    title: 'Reminders',
+    head: `<meta name="description" content="Invitations">
+           <script type="text/javascript">
+               function copy(message) {
+                    if ('clipboard' in navigator) {
+                        navigator.clipboard.writeText(message);
+                    } else {
+                        document.execCommand('copy', true, message);
+                    }
+                }
+            </script>
+    `,
+    body: generateBodyContent(urlListsReminders, hotelNightsList),
+});
+
+fs.writeFile('public/invitations.html', htmlInvitations, function (err) {
     if (err) throw err;
     console.log('Invitations page created');
 });
@@ -119,6 +146,16 @@ fs.writeFile('public/invitations.html', html, function (err) {
 fs.writeFile('public/invitations.json', JSON.stringify(urlLists, null, 2), (err) => {
     if (err) throw err;
     console.log('Invitations written to file');
+});
+
+fs.writeFile('public/reminders.html', htmlReminders, function (err) {
+    if (err) throw err;
+    console.log('Reminders page created');
+});
+
+fs.writeFile('public/reminders.json', JSON.stringify(urlListsReminders, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Reminders written to file');
 });
 
 console.log('This is after the write call');
